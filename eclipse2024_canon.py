e0aa2afa0a46692334d7acbd6562519af2ba47d9
Seemanta Dutta <seemanta@gmail.com>
2024-03-16 19:39:47 -0700

    Laptop commit 3/16 739pm

--- eclipse2024_canon.py
+++ eclipse2024_canon.py
@@ -17,6 +17,22 @@ MIN_STEP_SLOW = 1.000 # Verify your setup with USB updates. Gap between USB upda
 
 _1 = Exposure(1)
 
+# All times are UTC!!!
+# All times are UTC!!!
+# All times are UTC!!!
+script = Script(
+    #                 UTC                LOCAL TEXAS
+    c1  = "2024/04/08 17:21:27.5",     # 12:21:27.5 PM
+    c2  = "2024/04/08 18:38:46.6",     #  1:38:46.6 PM
+    max = "2024/04/08 18:40:58.0",     #  1:40:58.0 PM
+    c3  = "2024/04/08 18:43:09.4",     #  1:43:09.4 PM
+    c4  = "2024/04/08 20:01:20.9",     #  3:01:20.9 PM
+)
+
+script.camera = "C5d4"
+
+
+
 def _setup_for_partials(phase0, phase1):
     script.banner(f"{phase0} -> {phase1}: partials")
     script.phase = phase0
@@ -26,18 +42,16 @@ def _setup_for_partials(phase0, phase1):
     script.min_time_step = MIN_STEP_SLOW
     script.incremental = "N"
 
-def _diamond_ring(phase, offset, exposure):
-    script.banner(f"{phase} fast exposures for diamond ring & baily's beads.")
+def _diamond_ring(phase, offset, exposure, count = 1):
+    #script.banner(f"{phase} fast exposures for diamond ring & baily's beads.")
     script.phase = phase
     script.offset = offset
     script.iso = 100
     script.exposure = exposure
-    script.min_time_step = 0.66
+    script.min_time_step = MIN_STEP_FAST
     script.comment = "fast burst"
-    script.send_exposure()
-    script.offset += MIN_STEP_SLOW
     script.release_command = "TAKEPIC"
-    for _ in range(4):
+    for _ in range(count):
         script.capture()
         exposure *= 2
         script.exposure = exposure
@@ -340,7 +354,7 @@ def _uneclisped_sun_photos(phase, offset, count, delta, banner, expinfo):
     script.iso = expinfo.iso
     script.exposure = expinfo.shutter
     script.fstop = expinfo.fstop
-    script.min_time_step = 300
+    script.min_time_step = delta
     script.comment = "fast burst"
     script.release_command = "TAKEPIC"
     for _ in range(count):
@@ -355,94 +369,172 @@ if __name__ == '__main__':
     The EO script will have the following flow:
 
     1. Take 3 uneclipsed sun photos, to make sure focus and exposure are good. 
-    These should be done about 30mins before C1, spaced 5 mins apart
+    These should be done about20mins before C1, spaced 5 mins apart. DONE
 
-    2. Voice prompt for C1
+    2. Voice prompt for C1. DONE
     
-    2. Start partials, at every 1% progress starting at C1, through C2.
+    2. Start partials, at every 1% progress starting at C1, through C2. DONE
 
-    3. Battery change at around 90% progress in partials
+    3. Battery change at around 90% progress in partials. Add prompt? DONE
 
-    4. Voice prompt for filter removal
+    4. Voice prompt for filter removal. DONE
 
-    5. Take Bailey's beads and diamond ring shot close to C2
+    5. Take Bailey's beads and diamond ring shot close to C2 TODO
 
 
     5. At C2-2 seconds, SETEXP up the C2 exposure. At C2+3, start the long exposure sequence from 4s, 1s, .5s.
-       This will take 48 photos, bracketed exposure wise and will use RELEASE
+       This will take 48 photos, bracketed exposure wise and will use RELEASE. DONE
 
-    6. There is a TIME GAP from here until MAX (earthshine shots)
+    6. There is a TIME GAP from here until MAX (earthshine shots) TODO
 
     7. At MAX eclipse, the second half of bracket shots starting at 1/1000 are taken. This will be 56 shots, bracketed
-       exposure wise
+       exposure wise. DONE
 
-    8. There is a TIME GAP from here until C3 (Insurance shots)
+    8. There is a TIME GAP from here until C3 (Insurance shots) TODO
 
-    9. Take Bailey's beads and diamond ring close to C3
+    9. Take Bailey's beads and diamond ring close to C3 TODO
 
-    10. Continue with partials every 1% progress
+    10. Continue with partials every 1% progress. DONE
 
-    11. Battery change once more
+    11. Battery change once more. Add voice prompt? DONE
 
-    12. Uneclipsed Sun?
+    12. Uneclipsed Sun? No Need
 
     '''
 
 
-    # All times are UTC!!!
-    # All times are UTC!!!
-    # All times are UTC!!!
-    script = Script(
-        #                 UTC                LOCAL TEXAS
-        c1  = "2024/04/08 17:21:27.5",     # 12:21:27.5 PM
-        c2  = "2024/04/08 18:38:46.6",     #  1:38:46.6 PM
-        max = "2024/04/08 18:40:58.0",     #  1:40:58.0 PM
-        c3  = "2024/04/08 18:43:09.4",     #  1:43:09.4 PM
-        c4  = "2024/04/08 20:01:20.9",     #  3:01:20.9 PM
-    )
+    def gen_main_solar_eclispe_script():
+        script.fstop = 8
+        script.iso = 100
+        script.exposure = 1/125
 
-    script.camera = "C5d4"
+        # phase, offset to start taking photos, count of shots, difference between each shot, text banner, exposure info
+        expinfo.fstop = 8
+        expinfo.iso = 100
+        expinfo.shutter = 1/125
+        _uneclisped_sun_photos("C1", -1200, 3, 30, "UNECLIPSED SUN - test exposures in the field", expinfo)
 
-    # phase, offset to start taking photos, count of shots, difference between each shot, text banner, exposure info
-    expinfo.fstop = 8
-    expinfo.iso = 100
-    expinfo.shutter = 1/1000
-    _uneclisped_sun_photos("C1", -1200, 3, 300, "UNECLIPSED SUN - test exposures in the field", expinfo)
+        script.banner(f"C2 Baily\'s beads 1")
+        # BB 1
+        _diamond_ring("C2",-15, 1/4000)
+        _diamond_ring("C2",-14, 1/2000)
+        _diamond_ring("C2",-13, 1/1000)
+        _diamond_ring("C2",-12, 1/500)
+        _diamond_ring("C2",-11, 1/125)
+
+        # BB 2
+        script.banner(f"C2 Baily\'s beads 2")
+        _diamond_ring("C2",-10, 1/4000)
+        _diamond_ring("C2",-9, 1/2000)
+        _diamond_ring("C2",-8, 1/1000)
+        _diamond_ring("C2",-7, 1/500)
+        _diamond_ring("C2",-6, 1/125)
+
+        # DR 1, 2, 3
+        script.banner(f"C2 Diamond Ring")
+        _diamond_ring("C2",-5, 1/125)
+        _diamond_ring("C2",-4, 1/60)
+        _diamond_ring("C2",-3, 1/30)
+
+        # Chromosphere 1, 2
+        script.banner(f"C2 Chromosphere")
+        _diamond_ring("C2",-1, 1/2000)  #insurance
+        _diamond_ring("C2",0, 1/2000)
 
 
+        # C2->MAX cycle
+        _main_sequence("C2 sequence (C2->MAX)", "C2", 2, 2, 4, 1/500, "decreasing")
+        # Single exposure of 1/1000 centered at MAX
+        _main_sequence("MAX Single Shot", "MAX", -2, 2, 0.001, 0.001)
+        offset = script.offset
+        _main_sequence("MAX sequence (MAX->C3)", "MAX", offset, 2, 0.002, 4)
 
 
-    #_setup_for_partials("C1", "C2")
+        script.banner(f"C3 Chromosphere")
+        # Chromosphere 1, 2
+        _diamond_ring("C3",0, 1/2000)
+        _diamond_ring("C3",+1, 1/1000) #Insurance
 
-    # C2->MAX cycle
-    _main_sequence("C2 sequence (C2->MAX)", "C2", 2, 2, 4, 1/500, "decreasing")
+        script.banner(f"C3 Diamond Ring")
+        # DR 1, 2, 3
+        _diamond_ring("C3",+5, 1/125)
+        _diamond_ring("C3",+4, 1/60)
+        _diamond_ring("C3",+3, 1/30)
 
-    # Single exposure of 1/1000 centered at MAX
-    _main_sequence("MAX Single Shot", "MAX", -2, 2, 0.001, 0.001)
+        # BB 1
+        script.banner(f"C3 Baily's beads 1")
+        _diamond_ring("C3",+10, 1/4000)
+        _diamond_ring("C3",+9, 1/2000)
+        _diamond_ring("C3",+8, 1/1000)
+        _diamond_ring("C3",+7, 1/500)
+        _diamond_ring("C3",+6, 1/125)
 
-    offset = script.offset
-    _main_sequence("MAX sequence (MAX->C3)", "MAX", offset, 2, 0.002, 4)
+        # BB 2
+        script.banner(f"C3 Baily's beads 2")
+        _diamond_ring("C3",+15, 1/4000)
+        _diamond_ring("C3",+14, 1/2000)
+        _diamond_ring("C3",+13, 1/1000)
+        _diamond_ring("C3",+12, 1/500)
+        _diamond_ring("C3",+11, 1/125)
+
+        script.save("Eclipse2024CanonMain.csv")
 
 
+    def add_partial_progress_shots():
+        f = open("Eclipse2024CanonMain.csv", mode="a")
+        f.write(f"""# Partial Progress shots\n
+FOR,(VAR),1.000,1.000,99.900
+TAKEPIC,MAGPRE (VAR),+,00:00:00.0,C5d4,{script.exposure},{script.fstop},{script.iso},0.000,RAW,,N,Partials C1-C2, filter on
+ENDFOR
+FOR,(VAR),1.000,1.000,99.900
+TAKEPIC,MAGPOST (VAR),+,00:00:00.0,C5d4,{script.exposure},{script.fstop},{script.iso},0.000,RAW,,N,Partials C3-C4, filter on
+ENDFOR
+\n""")
+        f.close()
 
-
-
-    #_diamond_ring("C2", -10, 1/4000)
+    def add_voice_prompts():
+        f = open("Eclipse2024CanonMain.csv", mode="a")
+        f.write("PLAY,C2,-,00:14:35.0,Sounds/battery_change.wav,,,,,,,,\"Camera Battery Change\" voice prompt\n")
+        f.write("PLAY,C2,-,00:10:00.0,Sounds/10minutes.wav,,,,,,,,\"10 minutes\" voice prompt\n")
+        f.write("PLAY,C2,-,00:05:00.0,Sounds/5minutes.wav,,,,,,,,\"5 minutes\" voice prompt\n")
+        f.write("PLAY,C2,-,00:02:00.0,Sounds/2minutes.wav,,,,,,,,\"2 minutes\" voice prompt\n")
+        f.write("PLAY,C2,-,00:01:00.0,Sounds/60seconds.wav,,,,,,,,\"60 seconds\" voice prompt\n")
+        f.write("PLAY,C2,-,00:00:30.0,Sounds/30seconds.wav,,,,,,,,\"30 seconds\" voice prompt\n")
+        f.write("PLAY,C2,-,00:00:20.0,Sounds/filters_off.wav,,,,,,,,\"Filters off\" voice prompt\n")
+        f.write("PLAY,C2,-,00:00:10.0,Sounds/10seconds.wav,,,,,,,,\"10 seconds\" voice prompt\n")
+        f.write("PLAY,C3,+,00:00:20.0,Sounds/filters_on.wav,,,,,,,,\"Filters on\" voice prompt\n")
+        f.write("PLAY,C3,+,00:09:20.0,Sounds/battery_change.wav,,,,,,,,\"Filters on\" voice prompt\n")
+        f.close()
     
 
+    def gen_solar_filter_test():
+        ''' Solar filter Test '''
 
-    ''' Solar filter Test '''
-    '''
-    _diamond_ring("C2", -2, 1/1000)
+        script.fstop = 8
+        script.iso = 100
+        script.exposure = 1/1000
 
-    _diamond_ring("C2", +5, 1/8000)
+        _diamond_ring("C2", -2, 1/1000)
 
-    script.fstop = 16
-    _diamond_ring("C2", +10, 1/500)
+        _diamond_ring("C2", +5, 1/8000)
 
-    script.iso = 800
+        script.fstop = 16
+        _diamond_ring("C2", +12, 1/500)
+
+        script.iso = 800
+        script.fstop = 8
+        _diamond_ring("C2", +18, 1/1600)
+        
+        script.save("SolarFilterTest.csv")
+
+
+    #gen_solar_filter_test()
+    gen_main_solar_eclispe_script()
+    
+
     script.fstop = 8
-    _diamond_ring("C2", +15, 1/1600)
-    '''
-    script.save("eclipse2024_canon_main2024__.csv")
-    #script.save("SolarFilterTest.csv")
+    script.iso = 100
+    script.exposure = 1/125
+    #add_partial_progress_shots()
+
+    #add_voice_prompts()
