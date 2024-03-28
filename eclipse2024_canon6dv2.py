bbc791cf4b7d1c0288905b9919b1ca849f519c1a
Seemanta Dutta <seemanta@gmail.com>
2024-03-27 21:10:59 -0700

    Unmodded script

--- eclipse2024_canon6dv2.py
+++ eclipse2024_canon6dv2.py
@@ -567,7 +567,7 @@ ENDFOR
 
     script.fstop = 8
     script.iso = 100
-    script.exposure = 1/250  #use 250 when using the modded 6D
+    script.exposure = 1/125  #use 250 when using the modded 6D
     add_partial_progress_shots()
 
     add_voice_prompts()
